98b7ace786da4f8fb208874ddd5ce489ecce61eb
JulianVR09 <julianvillacis.02@gmail.com>
2024-11-15 21:13:27 -0500

    docs: fix incorrect response model in Swagger documentation

--- src/main.ts
+++ src/main.ts
@@ -16,7 +16,7 @@ async function bootstrap() {
   .addBearerAuth()
   .build();
 
-  const document = SwaggerModule.createDocument(app, config);
+  const document = () => SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api-docs', app, document)
 
   await app.listen(process.env.PORT || 3000);
